e94028aeccfdc70472b453c2ef2f072b40a287c7
Austin Slakey <austinslakey@Austins-MacBook-Pro.local>
2021-05-03 23:01:44 -0700

    inception and attention  models

--- fastmri/models/unet.py
+++ fastmri/models/unet.py
@@ -10,7 +10,7 @@ from torch import nn
 from torch.nn import functional as F
 
 
-class Unet(nn.Module):
+class Dilated_Unet(nn.Module):
     """
     PyTorch implementation of a U-Net model.
 
@@ -44,18 +44,18 @@ class Unet(nn.Module):
         self.num_pool_layers = num_pool_layers
         self.drop_prob = drop_prob
 
-        self.down_sample_layers = nn.ModuleList([ConvBlock(in_chans, chans, drop_prob)])
+        self.down_sample_layers = nn.ModuleList([DilatedInceptionBlock(in_chans, chans, drop_prob)])
         ch = chans
         for _ in range(num_pool_layers - 1):
-            self.down_sample_layers.append(ConvBlock(ch, ch * 2, drop_prob))
+            self.down_sample_layers.append(DilatedInceptionBlock(ch, ch * 2, drop_prob))
             ch *= 2
-        self.conv = ConvBlock(ch, ch * 2, drop_prob)
+        self.conv = DilatedInceptionBlock(ch, ch * 2, drop_prob)
 
         self.up_conv = nn.ModuleList()
         self.up_transpose_conv = nn.ModuleList()
         for _ in range(num_pool_layers - 1):
             self.up_transpose_conv.append(TransposeConvBlock(ch * 2, ch))
-            self.up_conv.append(ConvBlock(ch * 2, ch, drop_prob))
+            self.up_conv.append(DilatedInceptionBlock(ch * 2, ch, drop_prob))
             ch //= 2
 
         self.up_transpose_conv.append(TransposeConvBlock(ch * 2, ch))
@@ -104,7 +104,147 @@ class Unet(nn.Module):
 
         return output
 
+class Attn_Unet(nn.Module):
+    def __init__(self,
+                 in_chans: int,
+                 out_chans: int,
+                 chans: int = 32,
+                 num_pool_layers: int = 4,
+                 drop_prob: float = 0.0,):
+        
+        super().__init__()
+        
+        self.Maxpool = nn.MaxPool2d(kernel_size=2,stride=2)
 
+        self.Conv1 = ConvBlock(in_chans=in_chans, out_chans=64, drop_prob=drop_prob)
+        self.Conv2 = ConvBlock(in_chans=64, out_chans=128, drop_prob=drop_prob)
+        self.Conv3 = ConvBlock(in_chans=128, out_chans=256, drop_prob=drop_prob)
+        self.Conv4 = ConvBlock(in_chans=256, out_chans=512, drop_prob=drop_prob)
+        self.Conv5 = ConvBlock(in_chans=512, out_chans=1024, drop_prob=drop_prob)
+
+        self.Up5 = TransposeConvBlock(in_chans=1024, out_chans=512)
+        self.Att5 = Attention_block(F_g=512, F_l=512, F_int=256)
+        self.Up_conv5 = ConvBlock(in_chans=1024,  out_chans=512, drop_prob=drop_prob)
+
+        self.Up4 = TransposeConvBlock(in_chans=512, out_chans=256)
+        self.Att4 = Attention_block(F_g=256, F_l=256, F_int=128)
+        self.Up_conv4 = ConvBlock(in_chans=512,  out_chans=256, drop_prob=drop_prob)
+        
+        self.Up3 = TransposeConvBlock(in_chans=256, out_chans=128)
+        self.Att3 = Attention_block(F_g=128, F_l=128, F_int=64)
+        self.Up_conv3 = ConvBlock(in_chans=256,  out_chans=128, drop_prob=drop_prob)
+        
+        self.Up2 = TransposeConvBlock(in_chans=128, out_chans=64)
+        self.Att2 = Attention_block(F_g=64, F_l=64, F_int=32)
+        self.Up_conv2 = ConvBlock(in_chans=128, out_chans=64, drop_prob=drop_prob)
+
+        self.Conv_1x1 = nn.Conv2d(64, out_chans, kernel_size=1, stride=1, padding=0)
+
+
+    def forward(self,x):
+        # encoding path
+        x1 = self.Conv1(x)
+        x2 = self.Maxpool(x1)
+        x2 = self.Conv2(x2)
+        x3 = self.Maxpool(x2)
+        x3 = self.Conv3(x3)
+        x4 = self.Maxpool(x3)
+        x4 = self.Conv4(x4)
+        x5 = self.Maxpool(x4)
+        x5 = self.Conv5(x5)
+
+        # decoding + concat path
+        d5 = self.Up5(x5)
+        x4 = self.Att5(g=d5,x=x4)
+        d5 = torch.cat((x4,d5),dim=1)        
+        d5 = self.Up_conv5(d5)
+        d4 = self.Up4(d5)
+        x3 = self.Att4(g=d4,x=x3)
+        d4 = torch.cat((x3,d4),dim=1)
+        d4 = self.Up_conv4(d4)
+        d3 = self.Up3(d4)
+        x2 = self.Att3(g=d3,x=x2)
+        d3 = torch.cat((x2,d3),dim=1)
+        d3 = self.Up_conv3(d3)
+        d2 = self.Up2(d3)
+        x1 = self.Att2(g=d2,x=x1)
+        d2 = torch.cat((x1,d2),dim=1)
+        d2 = self.Up_conv2(d2)
+        d1 = self.Conv_1x1(d2)
+
+        return d1
+
+
+class Attention_block(nn.Module):
+    def __init__(self,F_g,F_l,F_int):
+        super(Attention_block,self).__init__()
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(F_int)
+            )
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1,stride=1,padding=0,bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+        self.relu = nn.ReLU(inplace=True)
+        
+    def forward(self,g,x):
+        g1 = self.W_g(g)
+        x1 = self.W_x(x)
+        psi = self.relu(g1+x1)
+        psi = self.psi(psi)
+
+        return x*psi
+
+
+class DilatedInceptionBlock(nn.Module):
+    
+    def __init__(self, in_chans: int, out_chans: int, drop_prob: float):
+        """
+        Args:
+            in_chans: Number of channels in the input.
+            out_chans: Number of channels in the output.
+            drop_prob: Dropout probability.
+        """
+        super().__init__()
+
+        self.in_chans = in_chans
+        self.out_chans = out_chans
+        
+        self.tower_0 = nn.Sequential(
+            nn.Conv2d(in_chans, out_chans, kernel_size=(1,1), padding=1, bias=False, dilation=(1,1)),
+            nn.InstanceNorm2d(out_chans),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        )
+        self.tower_1 = nn.Sequential(
+            nn.Conv2d(in_chans, out_chans, kernel_size=(1,1), padding=1, bias=False, dilation=(2,2)),
+            nn.InstanceNorm2d(out_chans),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        )
+        self.tower_2 = nn.Sequential(
+            nn.Conv2d(in_chans, out_chans, kernel_size=(1,1), padding=1, bias=False, dilation=(3,3)),
+            nn.InstanceNorm2d(out_chans),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+        )
+
+    def forward(self, image: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            image: Input 4D tensor of shape `(N, in_chans, H, W)`.
+
+        Returns:
+            Output tensor of shape `(N, out_chans, H, W)`.
+        """
+        out0 = self.tower_0(image)
+        out1 = self.tower_1(image)
+        out2 = self.tower_2(image)
+        return torch.cat([out0, out1, out2], dim=3)
+        
 class ConvBlock(nn.Module):
     """
     A Convolutional Block that consists of two convolution layers each followed by
